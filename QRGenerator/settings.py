bab98eb4d77b596bf14f3d575c752d02736a78a1
Souravm2003 <souravmahato4383@gmail.com>
2025-07-05 17:21:16 +0530

    adding procfile

--- QRGenerator/settings.py
+++ QRGenerator/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-o6zvi&k!9vjly5gm5-tcwttu5r@9b(p+$i-l)fqy8r93=xd2r&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -116,6 +116,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
